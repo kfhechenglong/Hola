5e4a7e19083362d7277ce0b3f94a9360ea98146e
hechenglong <kfhechenglong@126.com>
2022-11-04 10:36:11 +0800

    'deploy'

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,19 @@ set -e
 # 构建
 pnpm run docs:build
 
-# 进入组件目录
-cd packages/components
-# 构建组件
-pnpm run build
+# # 进入组件目录
+# cd packages/components
+# # 构建组件
+# pnpm run build
 
-cd ./../../
+cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
+git config user.name "hechenglong"
+git config user.email "kfhechenglong@126.com"
+# git init
 git add -A
 git commit -m 'build'
 
@@ -23,6 +26,6 @@ git commit -m 'build'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:kfhechenglong/Hola.git master:gh-pages
 
-cd -
+# cd -
