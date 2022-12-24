e619824f7a4000c88480c2e0c3da3edfdbc35229
hechenglong <kfhechenglong@126.com>
2022-12-24 16:27:21 +0800

    delete vue-quarter

--- packages/vue-quarter-picker/es/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { App } from 'vue';
-import QuarterSelect from './vue-quarter-picker.vue';
-export { QuarterSelect };
-declare const _default: {
-    install(app: App): void;
-};
-export default _default;
--- packages/vue-quarter-picker/lib/style.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const style = "";
-module.exports = style;
